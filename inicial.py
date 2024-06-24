2ed2ccccde73aa07ea34ead092a8002ade78b04c
Matheus <matheuslsdn2018@gmail.com>
2024-06-24 17:13:00 -0300

    versão 1.0.1

---
+++ inicial.py
@@ -0,0 +1,18 @@
+print("olá mundo")
+print("sou o negão do senac")
+print("já to com fome")
+
+print("matheus")
+print("almirante tamandaré")
+print(24)
+
+# meu primeiro python
+
+nome = input("digite seu nome: ")
+print("olá, " + nome)
+
+# digite seu nome
+
+nome = "matheus"
+sobrenome= "negão"
+print("olá, " + nome + " " + sobrenome)
