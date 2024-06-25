6d8ac6e5d3825d1a27f92d359aa4ebaf8ee63272
Matheus <matheuslsdn2018@gmail.com>
2024-06-25 14:13:34 -0300

    versão 1.0.2

--- inicial.py
+++ inicial.py
@@ -1,18 +1,29 @@
-print("olá mundo")
-print("sou o negão do senac")
-print("já to com fome")
+#prática 1
 
-print("matheus")
-print("almirante tamandaré")
-print(24)
+nome = "Tim Tester"
+idade = 20
+skill1 = "python"
+level1 = "iniciante"
+skill2 = "java"
+level2 = "senior"
+skill3 = "C++"
+level3 = "pleno"
+baixo = 2000
+alto = 3000
 
-# meu primeiro python
+print("meu nome é ", nome, "tenho", idade ,"anos")
+print("minhas habilidades são:")
+print(skill1, ":", level1)
+print(skill2, ":", level1)
+print(skill3, ":", level1)
 
-nome = input("digite seu nome: ")
-print("olá, " + nome)
+print("Procuro um emprego com um salário de", baixo, "a", alto, "euros por mês")
 
-# digite seu nome
+#Atividade 01
 
-nome = "matheus"
-sobrenome= "negão"
-print("olá, " + nome + " " + sobrenome)
+nome = "Steve"
+apelido = "Sanders"
+endereco = "Rua x"
+codPostal = 83225456
+
+print(nome,apelido,endereco,codPostal)
