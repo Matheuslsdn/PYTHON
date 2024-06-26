7e98f96789d040eda8c035a63e6062d6a46b772d
Matheus <matheuslsdn2018@gmail.com>
2024-06-26 16:03:23 -0300

    versão 1.0.7

--- inicial2.py
+++ inicial2.py
@@ -1,23 +1,29 @@
 #atividade 11
 
-capital = float(input("Qual o valor inicial? "))
-juros = float(input("Qual a taxa de juros anual? "))
-tempo = int(input("Quantos anos? "))
+#capital = float(input("Qual o valor inicial? "))
+#juros = float(input("Qual a taxa de juros anual? "))
+#tempo = int(input("Quantos anos? "))
 
-print(f"o montante final será de {capital + (capital * (juros / 100) * tempo)}")
+#print(f"o montante final será de {capital + (capital * (juros / 100) * tempo)}")
 
 #atividade 12
 
-num1 = float(input("Digite um número: "))
-num2 = float(input("Digite outro número: "))
-num3 = float(input("Digite outro número: "))
+#num1 = float(input("Digite um número: "))
+#num2 = float(input("Digite outro número: "))
+#num3 = float(input("Digite outro número: "))
 
-print(f"A média dos números é {(num1 + num2 + num3) / 3}")
+#print(f"A média dos números é {(num1 + num2 + num3) / 3}")
 
 #atividade 13
 
+# largura = float(input("Qual é a largura? "))
+# altura = float(input("Qual é a altura? "))
+
+# print(f"A área é de {largura * altura} e o perimetro é de {2 * (largura + altura)}")
 
 #atividade 14
 
+C = float(input("Qual é a temperatura atual? "))
 
+print(f"Convertido em fahrenheit { C * 9/5 + 32}")
 #atividade 15
