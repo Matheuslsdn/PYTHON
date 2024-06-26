bf980e91abded119a2e3bf165af162a7be566349
Matheus <matheuslsdn2018@gmail.com>
2024-06-26 14:59:08 -0300

    versão 1.0.7

--- inicial2.py
+++ inicial2.py
@@ -5,3 +5,19 @@ juros = float(input("Qual a taxa de juros anual? "))
 tempo = int(input("Quantos anos? "))
 
 print(f"o montante final será de {capital + (capital * (juros / 100) * tempo)}")
+
+#atividade 12
+
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+num3 = float(input("Digite outro número: "))
+
+print(f"A média dos números é {(num1 + num2 + num3) / 3}")
+
+#atividade 13
+
+
+#atividade 14
+
+
+#atividade 15
