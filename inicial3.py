0b7848fe3285e48c1b4ffadb683f757ab8e4dc98
Matheus <matheuslsdn2018@gmail.com>
2024-06-27 16:15:41 -0300

    versão 1.0.12

--- inicial3.py
+++ inicial3.py
@@ -1,18 +1,37 @@
-numero = int(input("Digite um número"))
+# numero = int(input("Digite um número"))
 
-if numero < 0:
-    print("O número é negativo")
+# if numero < 0:
+#     print("O número é negativo")
 
-if numero > 0:
-    print("O número é positivo")
+# if numero > 0:
+#     print("O número é positivo")
 
-if numero == 0:
-    print("O número é zero")
+# if numero == 0:
+#     print("O número é zero")
 
-#Prática
+# #Prática
 
-num = int(input("digite um número "))
+# num = int(input("digite um número "))
 
-if num == 1984:
-    print(f"{num} Orwell")
-print(num)
+# if num == 1984:
+#     print(f"{num} Orwell")
+# print(num) 
+
+#atividade 16
+
+# num1 = int(input("Digite um número "))
+# if num1 < 0: 
+#     print(f"{num1 * -1}")
+# else:
+#     print(num1)
+
+#atividade 17
+
+custo = 5.9
+nome = str(input("Qual o seu nome "))
+
+if nome == 'jerry':
+    print(custo)
+else:
+    porcoes = int(input("Quantas porções foram compradas "))
+    print(f"O valor da total da porção é de: {porcao * custo}")
