7b99db7e479f98f47e2c6f669c08b79ff9f9c8e1
Matheus <matheuslsdn2018@gmail.com>
2024-07-24 17:19:38 -0300

    versão 1.2.1

--- funçoes.py
+++ funçoes.py
@@ -1,8 +1,54 @@
-def media():
-    num1 = int(input("Digite um número: "))
-    num2 = int(input("Digite um número: "))
-    num3 = int(input("Digite um número: "))
-    media = (num1 + num2 + num3) / 3
-    print("A média dos números é: ", media)
+#Ativ 01
 
-media()
+# def media():
+#     num1 = int(input("Digite um número: "))
+#     num2 = int(input("Digite um número: "))
+#     num3 = int(input("Digite um número: "))
+#     media = (num1 + num2 + num3) / 3
+#     print("A média dos números é: ", media)
+
+# media()
+
+#Ativ 02
+
+# def printa_varias_vezes(texto, vezes):
+#     for i in range(vezes):
+#         print(texto)
+# texto = input("Digite um texto: ")
+# vezes = int(input("Digite quantas vezes deseja imprimir: "))
+        
+# printa_varias_vezes(texto, vezes);
+
+#Ativ 03
+
+# def quadrado_hashtag(tamanho):
+#     for i in range(tamanho):
+#         print("#" * tamanho)
+# tamanho = int(input("Digite o tamanho do quadrado: "))
+# quadrado_hashtag(tamanho)
+
+#Ativ 04
+
+# def mesa_xadrez():
+#     for x in range(8):
+#         for y in range(8):
+#             if (x + y) % 2 == 0:
+#                 print("1", end="")
+#             else:
+#                 print("0", end="")
+#         print()
+# mesa_xadrez()
+def mesa_xadrez(tamanho):
+    x = 0
+    while x < tamanho:
+        y = 0
+        while y < tamanho:
+            if (x + y) % 2 == 0:
+                print("1", end=" ")
+            else:
+                print("0", end=" ")
+            y += 1
+        print()
+        x += 1
+mesa_xadrez(40)
+
